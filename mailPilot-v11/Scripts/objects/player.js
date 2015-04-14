c2f05b29e9599017b0f0bda9dea6f52799b2ca89
AndrewMackle <andrewmackle@hotmail.com>
2015-04-14 11:36:09 -0400

    Created initial player object file

---
+++ mailPilot-v11/Scripts/objects/player.js
@@ -0,0 +1,13 @@
+﻿// player Class
+/*Source  file  name: player.ts, Author's  name: Andrew Mackle (300603655),  Last  Modified  by: Andrew Mackle,
+Date  last  Modified: 2015_04_14,  Program description： This is to crate the player object for the player to controll*/
+var objects;
+(function (objects) {
+    var Player = (function () {
+        function Player() {
+        }
+        return Player;
+    })();
+    objects.Player = Player;
+})(objects || (objects = {}));
+//# sourceMappingURL=player.js.map

592ea65c4469342859e71fad8b02fb0a8c6a586c
AndrewMackle <andrewmackle@hotmail.com>
2015-04-15 00:05:01 -0400

    Started creating player class with associated files
    -created player class
    -added constructor
    -added update with the code needed there for idle animation
    -added playerAsset object to house the assets of the player
    -added the player sprite sheet

--- mailPilot-v11/Scripts/objects/player.js
+++ mailPilot-v11/Scripts/objects/player.js
@@ -1,13 +1,51 @@
+﻿/// <reference path="../managers/playerasset.ts" />
 // player Class
 /*Source  file  name: player.ts, Author's  name: Andrew Mackle (300603655),  Last  Modified  by: Andrew Mackle,
 Date  last  Modified: 2015_04_14,  Program description： This is to crate the player object for the player to controll*/
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
 var objects;
 (function (objects) {
-    var Player = (function () {
+    var Player = (function (_super) {
+        __extends(Player, _super);
+        //the constructor of player class
         function Player() {
+            _super.call(this, managers.PlayerAssets.playerAtlas, "idle 0");
+            this.idleFrameRate = 100;
+
+            this.state = 0;
+            this.width = this.getBounds().width;
+            this.height = this.getBounds().height;
+            this.timeStart = Date.now();
+            this.currentFrame = 0;
+
+            this.regX = this.width * 0.5;
+            this.regY = this.height * 0.5;
         }
+        //Public methods
+        Player.prototype.update = function () {
+            this.elapsedTime = Date.now() - this.timeStart;
+            switch (this.state) {
+                case 0:
+                    //Idle animation
+                    if (this.elapsedTime >= this.idleFrameRate) {
+                        this.currentFrame++;
+                        if (this.currentFrame > 2) {
+                            this.currentFrame = 0;
+                        }
+                        this.gotoAndStop("idle " + this.currentFrame.toString());
+                    }
+                    break;
+                case 1:
+                    break;
+            }
+        };
         return Player;
-    })();
+    })(createjs.Sprite);
     objects.Player = Player;
 })(objects || (objects = {}));
 //# sourceMappingURL=player.js.map
--- mailPilot-v11/Scripts/objects/player.ts
+++ mailPilot-v11/Scripts/objects/player.ts
@@ -1,9 +1,51 @@
-﻿// player Class
+﻿/// <reference path="../managers/playerasset.ts" />
+// player Class
 /*Source  file  name: player.ts, Author's  name: Andrew Mackle (300603655),  Last  Modified  by: Andrew Mackle,  
 Date  last  Modified: 2015_04_14,  Program description： This is to crate the player object for the player to controll*/
 
 module objects {
-    export class Player {
+    export class Player extends createjs.Sprite {
+        //define propereties of player
+        width: number;
+        height: number;
+        state: number;
+        elapsedTime: number;
+        timeStart: number;
+        currentFrame: number;
+        idleFrameRate: number = 100;
 
+        //the constructor of player class
+        constructor() {
+            super(managers.PlayerAssets.playerAtlas, "idle 0");
+
+            this.state = 0;
+            this.width = this.getBounds().width;
+            this.height = this.getBounds().height;
+            this.timeStart = Date.now();
+            this.currentFrame = 0;
+
+            this.regX = this.width * 0.5;
+            this.regY = this.height * 0.5;
+        }
+
+        //Public methods
+        public update() {
+            this.elapsedTime = Date.now() - this.timeStart;
+            switch (this.state) {
+                case 0:
+                    //Idle animation
+                    if (this.elapsedTime >= this.idleFrameRate) {
+                        this.currentFrame++;
+                        if (this.currentFrame > 2) {
+                            this.currentFrame = 0;
+                        }
+                        this.gotoAndStop("idle " + this.currentFrame.toString());
+                    }
+                    break;
+                case 1:
+                    //moving animation
+                    break;
+            }
+        }
     }
 }

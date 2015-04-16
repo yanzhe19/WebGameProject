a919f53265c570a2da451910eda1cf87c340b7ee
AndrewMackle <andrewmackle@hotmail.com>
2015-04-16 13:26:54 -0400

    Fixed jump and land animations

--- mailPilot-v11/Scripts/objects/player.js
+++ mailPilot-v11/Scripts/objects/player.js
@@ -1,4 +1,4 @@
-/// <reference path="../managers/playerasset.ts" />
+﻿/// <reference path="../managers/playerasset.ts" />
 // player Class
 /*Source  file  name: player.ts, Author's  name: Andrew Mackle (300603655),  Last  Modified  by: Andrew Mackle,
 Date  last  Modified: 2015_04_14,  Program description： This is to crate the player object for the player to controll*/
@@ -15,16 +15,22 @@ var objects;
         //the constructor of player class
         function Player() {
             _super.call(this, managers.PlayerAssets.playerAtlas);
+
             this.state = "idle";
             this.gotoAndPlay(this.state);
             this.width = this.getBounds().width;
             this.height = this.getBounds().height;
+
             this.regX = this.width * 0.5;
             this.regY = this.height * 0.5;
+
             this.y = constants.GROUND_LEVEL;
             this.x = constants.GROUND_LEVEL * 0.5;
+
             onkeydown = this.keyDownEvent;
+
             //this.addEventListener("key down", this.handleClick);
+            this.player = this;
         }
         //event
         Player.prototype.keyDownEvent = function (event) {
@@ -32,11 +38,18 @@ var objects;
             var e = event.keyCode;
             switch (e) {
                 case 87:
-                    console.log("jump ... " + event);
-                    this.jump();
+                    //jump event
+                    console.log("jump event");
+                    player.jump();
+                    break;
+                case 83:
+                    //land event
+                    console.log("land event");
+                    player.land();
                     break;
             }
         };
+
         //Public methods
         Player.prototype.update = function () {
             switch (this.state) {
@@ -45,37 +58,48 @@ var objects;
                 case "jump":
                     //jump animation
                     console.log(this.y);
-                    this.y = constants.GROUND_LEVEL - Math.sin(Date.now() - this.timerStart);
+                    this.y = constants.GROUND_LEVEL - (Math.sin((Date.now() - this.timerStart) * 0.005) * 100);
                     console.log(this.y);
-                    if (this.y > constants.GROUND_LEVEL) {
-                        this.land;
-                        this.y = constants.GROUND_LEVEL;
+                    if (this.y > constants.GROUND_LEVEL + 15) {
+                        this.land();
+                        this.y = constants.GROUND_LEVEL + 15;
                     }
                     break;
                 case "land":
+                    //land animation
+                    if (Date.now() - this.timerStart >= 250) {
+                        this.y = constants.GROUND_LEVEL;
+                        this.idle();
+                    }
                     break;
             }
         };
+
         Player.prototype.land = function () {
+            this.timerStart = Date.now();
             this.state = "land";
-            this.gotoAndPlay(this.state);
+            player.gotoAndPlay(this.state);
         };
+
         Player.prototype.idle = function () {
             this.state = "idle";
             this.gotoAndPlay(this.state);
         };
+
         Player.prototype.walk = function () {
             this.state = "walk";
             this.gotoAndPlay(this.state);
         };
+
         Player.prototype.run = function () {
             this.state = "run";
             this.gotoAndPlay(this.state);
         };
+
         Player.prototype.jump = function () {
             this.state = "jump";
             this.timerStart = Date.now();
-            this.gotoAndPlay(this.state);
+            player.gotoAndPlay(this.state);
         };
         return Player;
     })(createjs.Sprite);

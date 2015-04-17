a849861e99c828c20fd328757d51beed6b7d4d0d
AndrewMackle <andrewmackle@hotmail.com>
2015-04-17 14:21:14 -0400

    set up basic structure to spell class

---
+++ mailPilot-v11/Scripts/objects/spell.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="../managers/playerasset.ts" />
+// spell Class
+/*Source  file  name: spell.ts, Author's  name: Andrew Mackle (300603655),
+Program description： This is to crate the spell object for the player to cast*/
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var objects;
+(function (objects) {
+    var Spell = (function (_super) {
+        __extends(Spell, _super);
+        //the constructor of spell class
+        function Spell(stateNumber) {
+            _super.call(this, managers.PlayerAssets.playerAtlas);
+            this.spell = this;
+        }
+        //Public methods
+        Spell.prototype.update = function () {
+        };
+
+        Spell.prototype.destroy = function () {
+        };
+        return Spell;
+    })(createjs.Sprite);
+    objects.Spell = Spell;
+})(objects || (objects = {}));
+//# sourceMappingURL=spell.js.map
--- mailPilot-v11/Scripts/objects/spell.ts
+++ mailPilot-v11/Scripts/objects/spell.ts
@@ -1,1 +1,28 @@
-﻿ 
+﻿/// <reference path="../managers/playerasset.ts" />
+// spell Class
+/*Source  file  name: spell.ts, Author's  name: Andrew Mackle (300603655),
+Program description： This is to crate the spell object for the player to cast*/
+
+module objects {
+    export class Spell extends createjs.Sprite {
+        //define propereties of player
+        width: number;
+        height: number;
+        spell: Spell;
+
+        //the constructor of spell class
+        constructor(stateNumber) {
+            super(managers.PlayerAssets.playerAtlas);
+            this.spell = this;
+        }
+
+        //Public methods
+        public update() {
+
+        }
+
+        public destroy() {
+
+        }
+    }
+}
